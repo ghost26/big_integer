a4d97267b0eb5f6450d94c44881163a57bc78825
Руслан Абдулхаликов <ruslanabdulhalikov@MacBook-Air-Ruslan.local>
2015-05-23 12:25:12 +0300

    Initial Commit

---
+++ big_integer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  big_integer
+//
+//  Created by Руслан Абдулхаликов on 23.05.15.
+//  Copyright (c) 2015 Руслан Абдулхаликов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
